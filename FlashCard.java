a33433419f143f87978aa66cc069760a713f2717
Beatriz de Souza <beatriz.branco@edu.fecap.br>
2025-02-13 19:20:30 -0300

    Ajustes na classe FlashCard
    Complementando a classe FlashCard com os setters e a

--- FlashCard.java
+++ FlashCard.java
@@ -2,15 +2,35 @@ public class FlashCard {
 
     private String questions;
     private String answers;
-    private int dificulty;
+    private int difficulty;
 
-    public FlashCard(String quetions, String answers){
+    public FlashCard(String questions, String answers){
         this.questions = quetions;
         this.answers = answers;
-        this.dificulty = 0;
+        this.difficulty = 0;
     }
 
+    //métodos getters
     public String getQuestions() {return questions;}
     public String getAnswers() {return answers;}
-    public int getDificulty() {return dificulty;}
+    public int getDificulty() {return difficulty;}
+
+    //métodos setters
+    public void setQuestion(String question){ this.question = question; }
+    public void setAnswer(String answer){ this.answer = answer; }
+    public void setDifficulty(int difficulty){ this.difficulty = difficulty; }
+
+    //método para editar o flashcard
+    public void editFlashCard(String newQuestion, String newAnswer, int newDifficulty){
+        this.question = newQuestion;
+        this.answer = newAnswer;
+        this.difficulty = newDifficulty;
+    }
+
+    //método para mostrar as informações do flashcard.
+    //é um protótipo, então faremos de maneira simples.
+    public String toString(){
+        return "Pergunta: " + question + "\nResposta: " + answer + "\nDificuldade: " + difficulty;
+    }
+
 }
