e842224c4ec84d7d3315d330ff560b0c0cc8146e
HeySatiroHere <Satirog719@gmail.com>
2025-03-29 23:54:30 -0300

    refatorando main

--- Flashcards/src/projeto_flashcards/Main.java
+++ Flashcards/src/projeto_flashcards/Main.java
@@ -1,71 +1,80 @@
-package projeto_flashcards;
+package Flashcards.src.projeto_flashcards;
 
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static Deck myDeck;
 
     //Fazendo a criação de cartas e de decks no terminal
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Deck meuDeck = null; // Começamos sem um deck criado
-
-        while(true){
-            System.out.println("BEM VINDO!");
+    public static void printOptions() {
         System.out.println("O que você deseja fazer?");
         System.out.println("1 - Criar meu deck");
         System.out.println("2 - Adicionar carta");
         System.out.println("3 - Visualizar Deck");
         System.out.println("4- Sair");
         System.out.print("Escolha uma opção: ");
+    }
 
-        int opcao = scanner.nextInt();
-        scanner.nextLine();
+    public static void createDeck() {
+        System.out.println("Ok, vamos criar seu deck!");
+        System.out.println("Qual sera o nome do Deck?");
+        String nomeDeck = scanner.nextLine();
+        myDeck = new Deck(nomeDeck);
+        System.out.println("Pronto! seu deck chamado " + nomeDeck + " está criado!");
+    }
 
-        switch(opcao){
-            case 1:
-                System.out.println("Ok, vamos criar seu deck!");
-                System.out.println("Qual sera o nome do Deck?");
-                String nomeDeck = scanner.nextLine();
-
-                meuDeck = new Deck(nomeDeck);
-
-                System.out.println("Pronto! seu deck chamado " + nomeDeck + " está criado!");
-                break;
-            case 2:
-                if(meuDeck == null){
-                    System.out.println("Antes de adicionar uma carta, crie o deck primeiro!");
-                    break;
-                }
-
-                System.out.println("Ok, vamos criar sua carta!");
-
-                System.out.println("Qual sera a pergunta?");
-                String pergunta = scanner.nextLine();
-
-                System.out.println("E qual será a resposta?");
-                String resposta = scanner.nextLine();
-                
-                FlashCard novoCard = new FlashCard(pergunta,resposta);
-                meuDeck.addFlashcard(novoCard);
-
-                System.out.println("Carta adicionada ao deck!");
-                break;
-            case 3:
-                if(meuDeck == null){
-                    System.out.println("Deck nao criado");
-                    break;
-                } else if (meuDeck.getFlashCards().isEmpty()) {
-                    System.out.println("Deck Vazio");
-                    break;
-                }
-                System.out.println(meuDeck.getFlashCards());
-                break;
-
-            case 4:
-                System.out.println("Saindo");
-                return;
+    public static void addCard() {
+        if (myDeck == null) {
+            System.out.println("Antes de adicionar uma carta, crie o deck primeiro!");
+            return;
         }
+        System.out.println("Ok, vamos criar sua carta!");
+        System.out.println("Qual sera a pergunta?");
+        String pergunta = scanner.nextLine();
+        System.out.println("E qual será a resposta?");
+        String resposta = scanner.nextLine();
+        FlashCard novoCard = new FlashCard(pergunta, resposta);
+        myDeck.addFlashcard(novoCard);
+        System.out.println("Carta adicionada ao deck!");
+    }
+
+    public static void viewDeck() {
+        if (myDeck == null) {
+            System.out.println("Deck nao criado");
+            return;
+        } else if (myDeck.getFlashCards().isEmpty()) {
+            System.out.println("Deck Vazio");
+            return;
+        }
+        System.out.println(myDeck.getFlashCards());
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("BEM VINDO!");
+        printOptions();
+
+        while (true) {
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+            switch (opcao) {
+                case 1:
+                    createDeck();
+                    printOptions();
+                    break;
+                case 2:
+                    addCard();
+                    break;
+                case 3:
+                    viewDeck();
+                    break;
+                case 4:
+                    System.out.println("Saindo");
+                    return;
+            }
         }
 
-}}
+    }
+}
